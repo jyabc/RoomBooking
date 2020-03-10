9500711a81ce645c9463fd7b4215b8e35f446411
jingying <jingying.lim@afusion.com>
2020-03-10 10:11:03 +0800

    add test

--- __tests__/Home-test.js
+++ __tests__/Home-test.js
@@ -1,15 +1,11 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import Home from '../src/Screen/Home';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-    const tree = renderer.create(<App />).toJSON();
+test('renders correctly', () => {
+    const tree = renderer.create(<Home />).toJSON();
     expect(tree).toMatchSnapshot();
-});
+  });
